d4a9dead65571708ec44e6aca2d3a10ed63246ad
Leandro Amarillo <leandroamarillo95@gmail.com>
2022-10-27 00:44:36 -0300

    fixed script

--- replace.sh
+++ replace.sh
@@ -6,7 +6,7 @@ compose="docker-compose.yml"
 read -p "Enter the DB_NAME: " db_name
 read -p "Enter the DB_PASS: " db_pass
 
-if [[ $db_name != "" && $db_user != "" && $db_pass != "" ]]; then
+if [[ $db_name != "" && $db_pass != "" ]]; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
         sed -i '' "s/DEFAULT_DB_NAME/$db_name/" $compose
         sed -i '' "s/DEFAULT_DB_PASS/$db_pass/" $compose
